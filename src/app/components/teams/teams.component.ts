f2649ed17f3aaccfaca91610d1969381e717b02d
Facundo Flores <flores.facundogabriel@exa.unsa.edu.ar>
2017-08-29 18:04:59 -0300

    Complete CRUD

--- src/app/components/teams/teams.component.ts
+++ src/app/components/teams/teams.component.ts
@@ -57,6 +57,53 @@ export class TeamsComponent implements OnInit {
     );
   }
 
+  getTeam(id: number = 0): void {
+    this.teamsService.getTeam(id)
+      .subscribe(
+      (team: ITeam) => this.onTeamRetrieved(team),
+    );
+  }
+
+  onTeamRetrieved(team: ITeam): void {
+    this.openedForm = true;
+    if (this.teamForm) {
+      this.teamForm.reset();
+    }
+    this.team = team;
+
+    if (this.team.id === '') {
+      this.pageTitle = 'Add Team';
+    } else {
+      this.pageTitle = `Edit Team: ${this.team.name}`;
+    }
+
+    // Update the data on the form
+    this.teamForm.patchValue({
+      name: this.team.name,
+      statusId: this.team.statusId
+    });
+  }
+
+  saveTeam() {
+    this.openedForm = false;
+    if (this.teamForm.dirty && this.teamForm.valid) {
+      const team = Object.assign({}, this.team, this.teamForm.value);
+
+      this.teamsService.saveTeam(team)
+        .subscribe(() => this.onSaveComplete());
+    }
+  }
+
+  delete(id: number): void {
+    this.teamsService.deleteTeam(id).subscribe(() => this.onSaveComplete());
+  }
+
+  onSaveComplete(): void {
+    // Reset the form to clear the flags
+    this.teamForm.reset();
+    this.refresh();
+  }
+
   private processError(error) {
     if (error.status === 401) {
       this.router.navigate(['/']);
--- src/app/services/teams.service.ts
+++ src/app/services/teams.service.ts
@@ -9,7 +9,8 @@ import { ITeam } from '../models/teams';
 @Injectable()
 export class TeamsService {
 
-  private baseUrl = `${environment.apiUrl}/api/teams`;
+  private filter = encodeURI('{\"include\"\:\"status\"}');
+  private baseUrl = `${environment.apiUrl}/api/teams?filter=${this.filter}`;
 
   constructor(private http: Http) { }
 
@@ -20,5 +21,72 @@ export class TeamsService {
     return this.http
       .get(this.baseUrl, options)
       .map(response => response.json());
+  }
+
+  getTeam(id: number): Observable<ITeam> {
+    const url = `${this.baseUrl} / ${id} `;
+    const accessToken = localStorage.getItem('accessToken');
+    const headers = new Headers({ 'Authorization': accessToken });
+    const options = new RequestOptions({ headers: headers });
+
+    if (id === 0) {
+      return Observable.create((observer: any) => {
+        observer.next(this.initializeTeam());
+        observer.complete();
+      });
+    } else {
+      return this.http
+        .get(url, options)
+        .map(response => response.json());
+    }
+  }
+
+  deleteTeam(id: number): Observable<Response> {
+    const accessToken = localStorage.getItem('accessToken');
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+      'Authorization': accessToken
+    });
+    const options = new RequestOptions({ headers: headers });
+
+    const url = `${this.baseUrl} /${id}`;
+    return this.http.delete(url, options)
+      .map(response => response.json());
+  }
+
+  saveTeam(team: ITeam): Observable<ITeam> {
+    const accessToken = localStorage.getItem('accessToken');
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+      'Authorization': accessToken
+    });
+    const options = new RequestOptions({ headers: headers });
+
+    if (team.id === '') {
+      return this.createTeam(team, options);
+    }
+    return this.updateTeam(team, options);
+  }
+
+  private createTeam(team: ITeam, options: RequestOptions): Observable<ITeam> {
+    team.id = undefined;
+    return this.http
+      .post(this.baseUrl, team, options)
+      .map(response => response.json());
+  }
+
+  private updateTeam(team: ITeam, options: RequestOptions): Observable<ITeam> {
+    const url = `${this.baseUrl}/${team.id}`;
+    return this.http
+      .put(url, team, options)
+      .map(response => response.json());
+  }
+
+  initializeTeam(): ITeam {
+    return {
+      id: '',
+      name: '',
+      statusId: ''
+    };
   }
 }
